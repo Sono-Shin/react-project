f46dc4c467f281a65c38b5a08d029f34b1a14f69
sh.shin <hoforyou@naver.com>
2026-02-03 14:22:59 +0900

    16 이벤트 핸들링 업로드

---
+++ event-handling/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './Button'
+
+function App() {
+
+  return (
+    <>
+      <Button name={'Home'} />
+      <Button name={'Store'} />
+      <Button name={'Contact'} />
+    </>
+  )
+}
+
+export default App
