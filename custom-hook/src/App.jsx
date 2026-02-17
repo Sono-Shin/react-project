eb9f556a600ae5934566a9c008eaaf3472464a25
Sono-Shin <hoforyou@naver.com>
2026-02-17 16:25:50 +0900

    22.customhook

---
+++ custom-hook/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import useWindowSize
+ from './hooks/useWindowSize'
+
+const App = () => {
+  const {width, height}
+   = useWindowSize()
+
+  return (
+    <>
+      <h2>Window Size</h2>
+      <p>Width: {width}</p>
+      <p>Height: {height}</p>
+    </>
+  )
+}
+
+export default App
