d54dc61858d5e43733257f09624726af7e7956bc
Sono-Shin <hoforyou@naver.com>
2026-02-17 16:48:16 +0900

    25 context

---
+++ context/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { useState }
+ from 'react'
+import Child1
+ from './Child1'
+
+function App() {
+  const [count, setCount]
+   = useState(0)
+
+  return (
+    <div>
+      <h2>App</h2>
+      <Child1
+        count={count} setCount={setCount} 
+        />
+    </div>
+  )
+}
+
+export default App
